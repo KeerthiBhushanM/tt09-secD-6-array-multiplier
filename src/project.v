e6661b39fe02d1f3cfc6b1846e16207b1e744735
Aiden Li <al6106@ad.nyu.edu>
2024-10-25 10:47:50 -0400

    bit len

--- src/project.v
+++ src/project.v
@@ -38,7 +38,7 @@ module part(
     output p);
     wire [2:0] w;
     
-    adder stage0 (m[0]&c,y[0],0,p,w[0]);
+    adder stage0 (m[0]&c,y[0],1'b0,p,w[0]);
     adder stage1 (m[1]&c,y[1],w[0],o[0],w[1]);
     adder stage2 (m[2]&c,y[2],w[1],o[1],w[2]);
     adder stage3 (m[3]&c,q4,w[2],o[2],co);
@@ -61,7 +61,7 @@ module array_mult_structural(
     assign p[5]=o4[1];
     assign p[6]=o4[2];
     assign p[7]=c[3];
-	part pa (m,3'b000,0,q[0],o1,c[0],p[0]);
+	part pa (m,3'b000,1'b0,q[0],o1,c[0],p[0]);
 	part pb (m,o1,c[0],q[1],o2,c[1],p[1]);
 	part pc (m,o2,c[1],q[2],o3,c[2],p[2]);
 	part pd (m,o3,c[2],q[3],o4,c[3],p[3]);
